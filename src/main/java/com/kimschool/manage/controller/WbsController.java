ab0178ab1b3762afe7d45f0d33b2a96ce3bb6a77
beemilly <beemillyjp@gmail.com>
2020-05-20 20:11:55 +0900

    Revert "Revert "5월 16일 숙제""
    This reverts commit f0074c377285c241f49166ebcbe3c659e9c4dad3.

--- src/main/java/com/kimschool/manage/controller/WbsController.java
+++ src/main/java/com/kimschool/manage/controller/WbsController.java
@@ -1,19 +1,49 @@
 package com.kimschool.manage.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kimschool.manage.entity.User_Info;
+import com.kimschool.manage.entity.Wbs_2020;
+import com.kimschool.manage.entity.Workplaceinfo;
+import com.kimschool.manage.service.WbsService;
+
 @Controller
 public class WbsController {
 
-	@RequestMapping("/wbs")
-	public ModelAndView init() {
+	@Autowired
+	WbsService service;
+	
+	
+//	@RequestMapping("/wbs123")
+//	public ModelAndView init() {
+//		
+//		ModelAndView mv = new ModelAndView("wbs");
+//		
+//		return mv;
+//	}
+	
+	@RequestMapping("wbslogincheck")
+	public ModelAndView wbslogincheck(String u_no, String u_password) {
 		
-		ModelAndView mv = new ModelAndView("wbs");
+		ModelAndView mv = service.wbslogincheck(u_no, u_password);
 		
 		return mv;
 	}
+	
+	@RequestMapping("getWbsInfo")
+	public ModelAndView getwbsinfo (String u_no) {
+		
+		ModelAndView mv = service.getwbsinfo(u_no);
+		
+		return mv;
+	}
+	
+	
 	
 	
 }
